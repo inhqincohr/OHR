ffb2e72fd95979140f692b25262927d8731a2450
inhqincohr <bryan.hopkins@outrigger.com>
2024-07-25 11:49:54 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
-// This code executes in its own worker or thread
-self.addEventListener("install", event => {
-   console.log("Service worker installed");
-});
-self.addEventListener("activate", event => {
-   console.log("Service worker activated");
+// The name of my cache
+const cacheName = "my-pwa-shell-v1.0";
+//The files I'm going to cache
+const filesToCache = [
+  "/",
+];
+
+self.addEventListener("install", e => {
+  console.log("[ServiceWorker] - Install");
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log("[ServiceWorker] - Caching app shell");
+    await cache.addAll(filesToCache);
+  })());
 });
