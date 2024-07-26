def48f2252a756d7f3e3586d35fbab541a71f948
inhqincohr <bryan.hopkins@outrigger.com>
2024-07-26 12:42:15 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,37 @@
-// The name of my cache
-const cacheName = "my-pwa-shell-v1.0";
-//The files I'm going to cache
-const filesToCache = [
-  "/",
-];
+const CACHE_NAME = 'cool-cache';
 
-self.addEventListener("install", e => {
-  console.log("[ServiceWorker] - Install");
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log("[ServiceWorker] - Caching app shell");
-    await cache.addAll(filesToCache);
-  })());
+// Add whichever assets you want to precache here:
+const PRECACHE_ASSETS = [
+    '/assets/',
+    '/src/'
+]
+
+// Listener for the install event - precaches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      // match the request to our cache
+      const cachedResponse = await cache.match(event.request);
+
+      // check if we got a valid response
+      if (cachedResponse !== undefined) {
+          // Cache hit, return the resource
+          return cachedResponse;
+      } else {
+        // Otherwise, go to the network
+          return fetch(event.request)
+      };
+  });
 });
